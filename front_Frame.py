a2b3e8b599e0b51096f69ce7f52629d2cf8b81e3
miku1001 <pauloted.22@gmail.com>
2023-07-10 00:46:42 +0800

    Adding buttons

--- front_Frame.py
+++ front_Frame.py
@@ -15,12 +15,17 @@ class FrontPage(tk.Tk):
     self.bg_image = ImageTk.PhotoImage(image)
 
     self.bg_label = tk.Label(self, image = self.bg_image)
-    self.bg_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+    self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# create buttons
-# add
-# search
+    # create buttons    
+    # add
+    self.add_button = tk.Button(self, text="Add Data", command=self.add, width=12, height=2, font=("Arial", 18))
+    self.add_button.pack(side=tk.BOTTOM, pady=10)
 
+    # search
+    self.search_button = tk.Button(self, text="Search", command=self.search, width=10, height=1,font=("Arial", 18))
+    self.search_button.pack(side=tk.BOTTOM, pady=5)
+    
 # create method for start button
 
 # create method for search button
