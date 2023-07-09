f161a3452ef1ce98ba43ca85d3f2fdb756f7f7ad
miku1001 <pauloted.22@gmail.com>
2023-07-10 00:26:54 +0800

    Creating class for front page

---
+++ front_Frame.py
@@ -0,0 +1,13 @@
+# import tkinter, pillow library
+
+# create class
+# create main window
+# create background image for front page
+
+# create buttons
+# add
+# search
+
+# create method for start button
+
+# create method for search button
