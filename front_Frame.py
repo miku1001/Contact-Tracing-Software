9f0f69ee18d03c24f74c33045c17406445bfffe0
miku1001 <pauloted.22@gmail.com>
2023-07-10 00:32:36 +0800

    Creating bg image for front page

--- front_Frame.py
+++ front_Frame.py
@@ -1,8 +1,21 @@
 # import tkinter, pillow library
+import tkinter as tk
+from PIL import ImageTk, image
 
 # create class
-# create main window
-# create background image for front page
+class FrontPage(tk.Tk):
+    # create main window
+    def __init__(self):
+        super().__init__()
+        self.title("Covid-19 Contact Tracing App")
+
+    # create background image for front page
+    image = Image.open("Insert Image here") # image underway
+    image = image.resize((900, 500), Image.NEAREST)
+    self.bg_image = ImageTk.PhotoImage(image)
+
+    self.bg_label = tk.Label(self, image = self.bg_image)
+    self.bg_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
 # create buttons
 # add
