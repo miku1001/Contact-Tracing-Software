192a9908001acf88aff62805d9a059f36f3fa0a0
miku1001 <pauloted.22@gmail.com>
2023-07-12 00:36:36 +0800

    Inserting text instruction

--- information_page.py
+++ information_page.py
@@ -3,10 +3,19 @@ import tkinter as tk
 from PIL import ImageTk, Image
 
 # Create class for Info page
-class InfoFrame(self):
-    tk.Frame.__init__(self)
+class InfoFrame(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
 
-    # Import bg image
-    image = Image.open("FFPAGE BG.png")
-    image = image.resize((900, 500), Image.NEAREST)
-    self.bg_image = ImageTk.PhotoImage(image)
+        # Import bg image
+        image = Image.open("FFPAGE BG.png")
+        image = image.resize((900, 500), Image.NEAREST)
+        self.bg_image = ImageTk.PhotoImage(image)
+
+        self.bg_label = tk.Label(self, image=self.bg_image)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Information label
+        self.info = tk.Label(self,text = "Please input your information. ", height=1, font=("Arial", 12))
+        self.info.place(x=30, y=10)
+        self.info.config(bg="#BAF8FA")
